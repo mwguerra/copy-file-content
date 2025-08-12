3b21c323d7237d6b57f64685b459e2635246ae78
availe <94802525+availe@users.noreply.github.com>
2025-08-12 16:06:39 -0400

    re-add markers

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.markdownToHTML
 
 fun properties(key: String) = providers.gradleProperty(key)
 fun environment(key: String) = providers.environmentVariable(key)
@@ -48,15 +49,19 @@ intellijPlatform {
             sinceBuild = properties("pluginSinceBuild")
         }
 
+
+        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         description = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-            val start = ""
-            val end = ""
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
 
             with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
-                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n")
+                subList(indexOf(start) + 1, indexOf(end))
+                    .joinToString("\n")
+                    .let(::markdownToHTML)
             }
         }
 
